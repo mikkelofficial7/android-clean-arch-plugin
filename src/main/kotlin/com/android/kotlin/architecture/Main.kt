ebc4d77251be459fc98b3ebe600f247ae8942304
Mikkel <mikkelseptiano86@gmail.com>
2026-03-01 22:38:11 +0700

    finishing plugin

--- src/main/kotlin/com/android/kotlin/architecture/Main.kt
+++ src/main/kotlin/com/android/kotlin/architecture/Main.kt
@@ -27,7 +27,12 @@ class Main : AnAction() {
             val repository = dialog.getRepositoryName()
 
             if (project != null && !useCase.isNullOrEmpty() && !repository.isNullOrEmpty() && !viewModel.isNullOrEmpty() && !activity.isNullOrEmpty()) {
-                FileGenerator(project, activity.toPascalCase(), viewModel.toPascalCase(), useCase.toPascalCase(), repository.toPascalCase()).execute()
+                val activityFormat = if (activity.any { it.isWhitespace() }) activity.toPascalCase() else activity
+                val viewModelFormat = if (viewModel.any { it.isWhitespace() }) viewModel.toPascalCase() else viewModel
+                val useCaseFormat = if (useCase.any { it.isWhitespace() }) useCase.toPascalCase() else useCase
+                val repositoryFormat = if (repository.any { it.isWhitespace() }) repository.toPascalCase() else repository
+
+                FileGenerator(project, activityFormat, viewModelFormat, useCaseFormat, repositoryFormat).execute()
                 SuccessAlertDialog().show()
             }
         }
